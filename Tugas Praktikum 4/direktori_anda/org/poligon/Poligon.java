b70e60bbf9f06db1390ecfb8d95342c48d874900
bismawira20 <117698894+bismawira20@users.noreply.github.com>
2024-03-18 10:42:37 +0700

    Add files via upload

---
+++ Tugas Praktikum 4/direktori_anda/org/poligon/Poligon.java
@@ -0,0 +1,13 @@
+/*File : Poligon.java
+ *Nama : Bisma Wira Adi Wicaksono
+  NIM : 24060122140120
+ */
+package direktori_anda.org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
